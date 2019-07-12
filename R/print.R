bafa495e5bbaeb01a279a5307fad60f179621953
Sam Toet <samanthactoet@gmail.com>
2019-07-12 15:38:04 -0400

    Print methods documentation

--- R/print.R
+++ R/print.R
@@ -1,3 +1,6 @@
+#' Print a leaflet map of results 
+#' 
+#' @param x Yelp business table created from search query
 #' @export
 print.business_tbl <- function(x, ..., view = interactive()) {
         x %>% leaflet::leaflet() %>%
@@ -8,6 +11,9 @@ print.business_tbl <- function(x, ..., view = interactive()) {
         invisible(x)
 }
 
+#' Print a table of the business listings
+#' 
+#' @param x Yelp business search results 
 #' @export
 print.tibble <- function(x, ..., view = interactive()) {
         class(x) <- class(tibble::tibble())
