052fcd5d000208ab4bd6f0bc709bd148e1084f44
Sam Toet <samanthactoet@gmail.com>
2019-08-07 16:19:48 -0400

    begin getting hours

---
+++ R/yelp_business.R
@@ -0,0 +1,41 @@
+#' @param id 
+#'
+#' @param client_secret 
+#'
+#' @export
+yelp_business <- function(id, client_secret = yelp_key("yelp")) {
+        
+        # Get client secret
+        client_secret <- get_secret(client_secret = client_secret)
+        
+        httr::RETRY(
+                verb = "GET",
+                url = paste0("https://api.yelp.com/v3/businesses/", id),
+                httr::add_headers(authorization = paste0("Bearer ", client_secret)))
+        
+        # If connection fails because of credential issues, show why
+        if (yelp_data %>% httr::status_code() >= 400 &
+            yelp_data %>% httr::status_code() < 500) {
+                
+                # Print error code
+                status_code <- yelp_data %>% httr::status_code()
+                stop("Cannot connect to the Yelp API. Status code is ", status_code, ".", call. = FALSE)
+        }
+        
+        # If connection works, get the data  
+        if (yelp_data %>% httr::status_code() >= 200 &
+            yelp_data %>% httr::status_code() < 300) {
+                
+                httr_content <- httr::content(yelp_data, as = "text")
+                
+                tbl <- 
+                        jsonlite::fromJSON(httr_content, flatten = TRUE)$businesses %>% 
+                        dplyr::as_tibble()
+                
+        # } else {
+        #         
+        #         business_tbl <- create_empty_business_tbl()
+        # }
+        }
+        tbl
+}
