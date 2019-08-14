24c04c6943f33534e691070a1b6fc7479d2c31ab
Sam Toet <samanthactoet@gmail.com>
2019-08-14 15:57:57 -0400

    Update yelp_business.R
    Get more data, including hours, for each business

--- R/yelp_business.R
+++ R/yelp_business.R
@@ -1,19 +1,22 @@
 #' Connect to Yelp and get a table of businesses
 #'
-#' With this function you can connect to Yelp using your API client secret or keyring. 
+#' This function connects to the Yelp API and gets data for a relevant business based on the business id.  
 #'
-#' The output is a large table containing relevant businesses.
+#' The output is
 #' 
 #' @param id The business id provided by Yelp
 #'
 #' @param client_secret Your Yelp API client secret
 #'
 #' @export
-yelp_business <- function(id, client_secret = yelp_key("yelp")) {
+yelp_businesses <- function(ids, client_secret = yelp_key("yelp")) {
         
         # Get client secret
         client_secret <- get_secret(client_secret = client_secret)
         
+        for (id in ids) {
+                
+        # Make the connection to Yelp
         yelp_data <- httr::RETRY(
                 verb = "GET",
                 url = paste0("https://api.yelp.com/v3/businesses/", id),
@@ -32,17 +35,21 @@ yelp_business <- function(id, client_secret = yelp_key("yelp")) {
         if (yelp_data %>% httr::status_code() >= 200 &
             yelp_data %>% httr::status_code() < 300) {
                 
+                # Retrieve the content 
                 httr_content <- httr::content(yelp_data, as = "text")
                 
-                tbl <- 
-                        jsonlite::fromJSON(httr_content, flatten = TRUE)$businesses %>% 
+                # Convert to JSON and store in tbl
+                tbl <- jsonlite::fromJSON(httr_content, flatten = TRUE)$hours %>% 
                         dplyr::as_tibble()
+                browser()
                 
         # } else {
         #         
         #         business_tbl <- create_empty_business_tbl()
         # }
         }
+        }
+        # Print the table
         tbl
 }
 
