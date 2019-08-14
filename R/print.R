3563fa8226fed9d4dea8416da374ca83cbcb5929
Sam Toet <samanthactoet@gmail.com>
2019-08-14 15:56:18 -0400

    Add address and rating to popup

--- R/print.R
+++ R/print.R
@@ -5,12 +5,14 @@
 #' @param view Interactive
 #' @export
 print.business_tbl <- function(x, ..., view = interactive()) {
+        popup <- paste0("<p style=\"text-align:center;\"><strong>", x$name, "</strong>", "<br>", "Rating: ", x$rating, "<br>", x$address, "</p>")
+        # popup <- htmltools::tags$p(x$name) %>% as.character() - try with for loop
         x %>% leaflet::leaflet() %>%
                 leaflet::addProviderTiles(
                         provider = "Stamen.TonerLite",
                         group = "Stamen Toner"
                 ) %>%
-                leaflet::addMarkers(lng = x$lon, lat = x$lat, popup = x$name) %>%
+                leaflet::addMarkers(lng = x$lon, lat = x$lat, popup = popup) %>%
                 print(browse = view, ...)
         print.tibble(x)
         invisible(x)
