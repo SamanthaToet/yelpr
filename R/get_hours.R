a61f6b560922fd414cb30bbe4f18b2e9e888a3fc
Sam Toet <samanthactoet@gmail.com>
2019-10-02 16:20:36 -0400

    Filter hours by business name

--- R/get_hours.R
+++ R/get_hours.R
@@ -1,21 +1,22 @@
 #' Print a table of hours for a business
 #'
-#' @param tbl 
-#' @param client_secret 
+#' This is a helper function
+#'
+#' @param tbl the results of yelp_search
 #'
 #' @return
 #' @export
 #'
 #' @examples
-get_hours <- function(tbl, client_secret = yelp_key("yelp")) {
-        
-        # Get client secret
-        client_secret <- get_secret(client_secret = client_secret)
-        
-        # Get business table
-        yelp_businesses()
-        
-        # Get the business name and id from that df
-        
-        # Use the id to print the hours 
+get_hours <- function(tbl, business_name = NULL) {
+       
+        if (!is.null(business_name)) {
+                tbl %>%
+                dplyr::filter(name %in% business_name) -> tbl
+        }
+
+        tbl %>%
+                dplyr::select(name, hours) %>%
+                tidyr::unnest()
 }
+
