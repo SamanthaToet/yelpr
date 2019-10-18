22cda3d82637dadf194fd82bd39e6b476105d999
Sam Toet <samanthactoet@gmail.com>
2019-10-18 10:15:23 -0400

    Start scraping reviews

---
+++ R/reviews.R
@@ -0,0 +1,12 @@
+#' Scrape Reviews
+#'
+#' @param pagination 
+#'
+#' @return
+#' @noRd
+scrape_reviews <- function(pagination) {
+        pagination <- c(0)
+        browser()
+        yelp_html <- read_html(paste0("", pagination))
+}
+     
