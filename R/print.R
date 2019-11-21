a135dac40e605b9e0a263aa6e10a3cdfdbac1ec5
Sam Toet <samanthactoet@gmail.com>
2019-11-21 16:46:41 -0500

    First attempt at https://github.com/SamanthaToet/yelpr/issues/13

--- R/print.R
+++ R/print.R
@@ -1,13 +1,17 @@
-#' Print a leaflet map of results 
+#' Print a Leaflet map of results 
 #' 
 #' @param x Yelp business table created from search query.
 #' @param ... Optional arguments to print.
-#' @param view Interactive leaflet map
+#' @param view Interactive leaflet map.
+#' 
+#' @return Returns a table of relevant businesses in the console and prints an interactive Leaflet map in the Viewer tab. Relevant businesses are marked. 
 #' 
 #' @export
-print.business_tbl <- function(x, ..., view = interactive()) {
+print.business_tbl <- function(x, ..., view = interactive(), show_map = getOption("yelpr.show_map")) {
         popup <- paste0("<p style=\"text-align:center;\"><strong>", x$name, "</strong>", "<br>", "Rating: ", x$rating, "<br>", x$address, "</p>")
-        #popup <- htmltools::tags$p(x$name) %>% as.character() - try with for loop
+        #TODO: popup <- htmltools::tags$p(x$name) %>% as.character() - try with for loop
+        
+        if (isTRUE(show_map) || is.null(show_map)) {
         x %>% leaflet::leaflet() %>%
                 leaflet::addProviderTiles(
                         provider = "Stamen.TonerLite",
@@ -16,4 +20,5 @@ print.business_tbl <- function(x, ..., view = interactive()) {
                 leaflet::addMarkers(lng = x$lon, lat = x$lat, popup = popup) %>%
                 print(browse = view, ...)
         NextMethod()
+        }
 }
